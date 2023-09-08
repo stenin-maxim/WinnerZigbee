febcca12d11b174b1241d56cc71572d549f0ac85
Maxim <stenin.dev@gmail.com>
2023-09-08 16:18:56 +0300

    Add wired sensor and radio sensor in interface

--- src/pages/sensors/index.tsx
+++ src/pages/sensors/index.tsx
@@ -1,58 +1,123 @@
 import React from 'react';
-import { View, Button } from "@ray-js/components";
+import { View, Button, Icon, Text } from "@ray-js/components";
 import styles from './index.module.less';
-import { useProps, useDevice } from '@ray-js/panel-sdk';
+import { useProps} from '@ray-js/panel-sdk';
 
 export default () => {
-    let device = useDevice();
-    let wiredSensors = [
-        {
-            "id": "1",
-            "online": true,
-            "name": "wired_sensor_1",
-            "leak": false,
-        },
-        {
-            "id": "2",
-            "online": true,
-            "name": "wired_sensor_2",
-            "leak": false,
+    let countWiredSensors: number = 0;
+    let countRadioSensors: number = 0;
+    let wiredSensors = useProps((props) => {
+        let wiredSensors = [];
+
+        if (props.wired_sensor_1 !== null) {
+            wiredSensors.push(JSON.parse(props.wired_sensor_1));
         }
-    ];
-    
-    device.dpSchema.radio_sensors = [];
 
-    let radioSensors = [];
+        countWiredSensors = wiredSensors.length;
 
-/*    let info = getDeviceInfo();
-
-    console.log(info.dps);
-    console.log(dpSchema);*/
-
-
-    const test = useProps((props) => {
-        //props.test = wiredSensor;
-        //console.log(props.test);
-        device.dpSchema.wired_sensors = wiredSensors;
-        return device.dpSchema.wired_sensors;
+        return wiredSensors;
     });
 
-    function addSensors(): void
+    let radioSensors = useProps((props) => {
+        let radioSensors = [];
+
+        if (props.radio_sensor_1 !== null) {
+            radioSensors.push(JSON.parse(props.radio_sensor_1));
+        }
+
+        if (props.radio_sensor_2 !== null) {
+            radioSensors.push(JSON.parse(props.radio_sensor_2));
+        }
+
+        if (props.radio_sensor_3 !== null) {
+            radioSensors.push(JSON.parse(props.radio_sensor_3));
+        }
+
+        countRadioSensors = radioSensors.length;
+
+        return radioSensors;
+    });
+    
+    function showWiredSensors()
     {
-        //device.dpSchema.radio_sensors = [];
-        device.dpSchema.radio_sensors.push({id: 1, name: 'red', leak: false, battery: 50, signal: 90});
+        return (
+            wiredSensors.map((item, index) => {
+                return (
+                    <React.Fragment key={index}>
+                        <View
+                        className={styles.sensor}
+                        style={{ border: item.leak ? '1px solid #FF0000' : '1px solid white' }}
+                        >
+                            <View>       
+                                <View style={{ display: item.leak ? 'inline-block' : 'none' }}>
+                                    <Icon type="icon-warning" color="#FF0000" size="26"></Icon>
+                                </View>
+                                <View style={{ display: item.leak ? 'none' : 'inline-block' }}>
+                                    <Icon type="icon-a-sunminfill" color="black" size="26"></Icon>
+                                </View>
+                                <Text className={styles.name}>{ item.name }</Text>
+                            </View>
+                        </View>
+                    </React.Fragment>
+                )
+            })
+        )
     }
 
-    console.log(device.dpSchema.radio_sensors);
-    //console.log(device.dpSchema.test);
-    console.log(test);
-    //console.log(radioSensors);
+    function showRadioSensors()
+    {
+        return (
+            radioSensors.map((item, index) => {
+                return (
+                    <React.Fragment key={index}>
+                        <View
+                        className={styles.sensor}
+                        style={{ border: item.leak ? '1px solid #FF0000' : '1px solid white' }}
+                        >
+                            <View>
+                                <View style={{ display: item.leak ? 'inline-block' : 'none' }}>
+                                    <Icon type="icon-warning" color="#FF0000" size="26"></Icon>
+                                </View>
+                                <View style={{ display: item.leak ? 'none' : 'inline-block' }}>
+                                    <Icon type="icon-a-sunminfill" color="black" size="26"></Icon>
+                                </View>
+                                <Text className={styles.name}>{ item.name }</Text>
+                            </View>
+                            <View className={styles.signalBattery}>
+                                <View className={styles.signal}>
+                                    <Icon type="icon-wifi" color="black" size="26"/>
+                                    <Text className={styles.signalText}>{ item.signal }</Text>
+                                </View>
+                                <View className={styles.battery}>
+                                    <Icon type="icon-a-boltfill" color="black" size="26"/>
+                                    <Text className={styles.batteryText}>{ item.battery }</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </React.Fragment>
+                )
+            })
+        )
+    }
 
     return (
-        <View className={styles.body}>
-            Настройки
+        <View>
+            <Text className={styles.title}>Количество добавленных:</Text>
+            <View className={styles.wrapSensors}>
+                <View className={styles.border}>
+                    <Text>Проводных датчиков:</Text>
+                    <Text className={styles.countSensors}>{ countWiredSensors }</Text>
+                </View>
+                <View>
+                    <Text>Беспроводных датчиков:</Text>
+                    <Text className={styles.countSensors}>{ countRadioSensors }</Text>
+                </View>
+            </View>
+
+            { countWiredSensors ? showWiredSensors() : '' }
+            { countRadioSensors ? showRadioSensors() : '' }
+
             <Button onClick={() => {
-                addSensors();
             }}>Добавить</Button>
         </View>
     );
