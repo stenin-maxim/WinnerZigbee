b6a49f7c43f954947c15302f6af736baa62e6106
Maxim <stenin.dev@gmail.com>
2023-10-11 11:06:40 +0300

    Refactor instruction

--- src/pages/instructions/index.config.ts
+++ src/pages/instructions/index.config.ts
@@ -1,14 +1,14 @@
 export const web = {
     backgroundColor: '#f2f4f6',
-    navigationBarTitleText: 'Инструкция пользователя',
+    navigationBarTitleText: '',
 };
 
 export const wechat = {
     backgroundColor: '#f2f4f6',
-    navigationBarTitleText: 'Инструкция пользователя',
+    navigationBarTitleText: '',
 };
 
 export const tuya = {
     backgroundColor: '#f2f4f6',
-    navigationBarTitleText: 'Инструкция пользователя',
+    navigationBarTitleText: '',
 };
