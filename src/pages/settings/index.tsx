fcf537dc7ff7a70b504f98ee9cd68b5192b4ae6a
Maxim <stenin.dev@gmail.com>
2024-02-02 15:20:59 +0300

    Refactor page settings

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -6,16 +6,34 @@ import Strings from '../../i18n';
 
 export default () => {
     let textInstruction: string = Strings.getLang('instruction');
+    let textСounter1: string = 'Счетчик 1';
+    let textСounter2: string = 'Счетчик 2';
 
     return (
         <View>
             <View className={styles.link} 
-                onClick={() => navigateTo({ url: '/pages/instructions/index'})}
+                onClick={() => navigateTo({ url: '/pages/instruction/index'})}
             >
-                <Icon type="icon-a-exclamationmarkbubblefill" size={26}></Icon>
+                <Icon type="icon-a-exclamationmarkbubblefill" size={26} color="#00BFFF"></Icon>
                 <Text className={styles.linkText}>{textInstruction}</Text>
+                <Icon type="icon-right" size={18}></Icon>
+            </View>
+            <View className={styles.link} 
+                onClick={() => navigateTo({ url: '/pages/counters/index'})}
+            >
+                <Icon type="icon-a-dropfill" size={26} color="#00BFFF"></Icon>
+                <Text className={styles.linkText}>{textСounter1}</Text>
+                <Icon type="icon-right" size={18}></Icon>
+            </View>
+            <View className={styles.link} 
+                onClick={() => navigateTo({ url: '/pages/counters/index'})}
+            >
+                <Icon type="icon-a-dropfill" size={26} color="#00BFFF"></Icon>
+                <Text className={styles.linkText}>{textСounter2}</Text>
                 <Icon type="icon-right" size={18}></Icon>
             </View>
         </View>
     );
+
+    
 }
