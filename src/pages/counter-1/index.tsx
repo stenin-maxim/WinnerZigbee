92bfe1b69a3d51793f7ef05f3dc463ae2373e04c
Maxim <stenin.dev@gmail.com>
2024-02-20 13:49:41 +0300

    Refactor counter-1 and counter-2

--- src/pages/counter-1/index.tsx
+++ src/pages/counter-1/index.tsx
@@ -2,111 +2,33 @@ import React from 'react';
 import { View, Text, RadioGroup, Radio, Label, Input, Button } from '@ray-js/ray';
 import { useProps, useActions } from '@ray-js/panel-sdk';
 import styles from '@/pages/counters/index.module.less';
-import Strings from '../../i18n';
+import * as counter from '@/components/counter';
 
 export default () => {
     const ACTIONS: any = useActions();
     let multiplier1: string = useProps((props): string => String(props.weather_delay));
     let counter1: number = useProps((props): number => Number(props.countdown));
     let [valueCounter1, setCounter1] = React.useState('');
-    let textIndicatorCounter: string = Strings.getLang('text_indicator_counter'),
-        textSettingCounter: string = Strings.getLang('text_setting_counter'),
-        textImpulsLiter: string = Strings.getLang('text_impuls_liter'),
-        textImpuls10Liter: string = Strings.getLang('text_impuls_10_liter'),
-        textSave: string = Strings.getLang('text_save');
+    let viewCounter = counter.viewCounter(counter1, multiplier1);
 
-    function handleInput2(event: any): void
+    function handleInput1(event: any): void
     {
-        setCounter1(addPoint(event.value));
+        setCounter1(counter.addPoint(event.value));
     }
 
-    /**
-     * Добавление точки к числу
-     * 
-     * @param eventValue 
-     * @returns 
-     */
-    function addPoint(eventValue: string) {
-        let arr: string[] = String(eventValue).split('');
-        let str: string;
-    
-        if (arr.length == 5) {
-            arr.push('.');
-        }
-        str = arr.join('');
-    
-        return str;
-    }
-
-    /**
-     * Получение параметра счетчика
-     * 
-     * @param valueCounter 
-     * @param multiplier 
-     * @returns 
-     */
-    function getCounter(valueCounter: string, multiplier: string): number
+    function saveCounter(): void
     {
-        let value = valueCounter.replace(".", "");
-        let counter: number;
-
-        if (multiplier === '10') {
-            counter = Math.round(Number(value) / 10);
-        } else {
-            counter = Number(value);
-        }
-
-        return counter;
-    } 
-
-    function saveCounter1(): void // TODO
-    {
-        let counter = getCounter(valueCounter1, multiplier1);
+        let value = counter.getCounter(valueCounter1, multiplier1);
 
         setCounter1('');
-        ACTIONS.countdown.set(counter);
+        ACTIONS.countdown.set(value);
     }
 
-    const changeRadio1 = (e) => {
+    const changeRadio1 = (e: any) => {
         if (typeof e.detail.value === "string") {
             ACTIONS.weather_delay.set(e.detail.value);
         }
 	};
-
-    /**
-     * Показать счетчик
-     * @param counter - показатель счетчика
-     * @param multiplier - импульс счетчика
-     * @returns object
-     */
-    function viewCounter(counter: number, multiplier: string): object
-    {
-        let counterMultiplier = counter * Number(multiplier);
-        let arr: string[] = String(counterMultiplier).split('');
-        let str1: string, str2: string;
-
-        if (arr.length < 8) {
-            for (let i = arr.length; i < 8; i++) {
-                arr.unshift('0');
-            }
-        }
-        str1 = arr.slice(0, 5).join('');
-        str2 = arr.slice(5, 8).join('');
-        
-        return (
-            <View className={styles.indicatorCounter}>
-                <View className={styles.indicatorCounterBlock}>
-                    <Text>{str1}</Text>
-                    <Text>.</Text>
-                    <Text className={styles.threeNumber}>{str2}</Text>
-                </View>
-                <View className={styles.meterCube}>
-                    <Text>м</Text>
-                    <Text className={styles.cube}>3</Text>
-                </View>
-            </View>
-        );
-    }
 
     function viewImpuls(multiplier: string): object
     {
@@ -114,10 +36,10 @@ export default () => {
             return (
                 <React.Fragment>
                     <Label>
-                        <Radio value="1" color="#00BFFF" checked>{textImpulsLiter}</Radio>
+                        <Radio value="1" color="#00BFFF" checked>{counter.textImpulsLiter}</Radio>
                     </Label>
                     <Label>
-                        <Radio value="10" color="#00BFFF">{textImpuls10Liter}</Radio>
+                        <Radio value="10" color="#00BFFF">{counter.textImpuls10Liter}</Radio>
                     </Label>
                 </React.Fragment>
             )
@@ -125,10 +47,10 @@ export default () => {
             return (
                 <React.Fragment>
                     <Label>
-                        <Radio value="1" color="#00BFFF">{textImpulsLiter}</Radio>
+                        <Radio value="1" color="#00BFFF">{counter.textImpulsLiter}</Radio>
                     </Label>
                     <Label>
-                        <Radio value="10" color="#00BFFF" checked>{textImpuls10Liter}</Radio>
+                        <Radio value="10" color="#00BFFF" checked>{counter.textImpuls10Liter}</Radio>
                     </Label>
                 </React.Fragment>
             )
@@ -137,27 +59,35 @@ export default () => {
 
     return (
         <View>
-            <View>
-                <Text className={styles.title}>{textIndicatorCounter}</Text>
-                <Text>1:</Text>
-                { viewCounter(counter1, multiplier1) }
-                <Text className={styles.title}>{textSettingCounter}</Text>
-                <Text>1:</Text>
-                <RadioGroup onChange={changeRadio1} options={[]} className={styles.radioGroup}>
-                    {viewImpuls(multiplier1)}
-                </RadioGroup>
-                <View className={styles.editCounter}>
-                    <Input type="digit"
-                        value={valueCounter1}
-                        maxLength={9}
-                        placeholder="00000.000"
-                        className={styles.inputNumber}
-                        onInput={handleInput2}
-                    ></Input>
-                    <Button className={styles.buttonSave} onClick={ () => {
-                        saveCounter1();
-                    }}>{textSave}</Button>
+            <Text className={styles.title}>{counter.textIndicatorCounter}</Text>
+            <Text>1:</Text>
+            <View className={styles.indicatorCounter}>
+                <View className={styles.indicatorCounterBlock}>
+                    <Text>{viewCounter[0]}</Text>
+                    <Text>.</Text>
+                    <Text className={styles.threeNumber}>{viewCounter[1]}</Text>
                 </View>
+                <View className={styles.meterCube}>
+                    <Text>м</Text>
+                    <Text className={styles.cube}>3</Text>
+                </View>
+            </View>
+            <Text className={styles.title}>{counter.textSettingCounter}</Text>
+            <Text>1:</Text>
+            <RadioGroup onChange={changeRadio1} options={[]} className={styles.radioGroup}>
+                {viewImpuls(multiplier1)}
+            </RadioGroup>
+            <View className={styles.editCounter}>
+                <Input type="digit"
+                    value={valueCounter1}
+                    maxLength={9}
+                    placeholder="00000.000"
+                    className={styles.inputNumber}
+                    onInput={handleInput1}
+                ></Input>
+                <Button className={styles.buttonSave} onClick={ () => {
+                    saveCounter();
+                }}>{counter.textSave}</Button>
             </View>
         </View>
     );
--- src/pages/counter-2/index.tsx
+++ src/pages/counter-2/index.tsx
@@ -2,98 +2,33 @@ import React from 'react';
 import { View, Text, RadioGroup, Radio, Label, Input, Button } from '@ray-js/ray';
 import { useProps, useActions } from '@ray-js/panel-sdk';
 import styles from '@/pages/counters/index.module.less';
-import Strings from '../../i18n';
+import * as counter from '@/components/counter';
 
 export default () => {
     const ACTIONS: any = useActions();
     let multiplier2: string = useProps((props): string => String(props.smart_weather));
     let counter2: number = useProps((props): number => Number(props.minihum_set));
     let [valueCounter2, setCounter2] = React.useState('');
-    let textIndicatorCounter: string = Strings.getLang('text_indicator_counter'),
-        textSettingCounter: string = Strings.getLang('text_setting_counter'),
-        textImpulsLiter: string = Strings.getLang('text_impuls_liter'),
-        textImpuls10Liter: string = Strings.getLang('text_impuls_10_liter'),
-        textSave: string = Strings.getLang('text_save');
+    let viewCounter = counter.viewCounter(counter2, multiplier2);
 
     function handleInput2(event: any): void
     {
-        setCounter2(addPoint(event.value));
+        setCounter2(counter.addPoint(event.value));
     }
 
-    function addPoint(eventValue: string) {
-        let arr: string[] = String(eventValue).split('');
-        let str: string;
-
-        if (arr.length == 5) {
-            arr.push('.');
-        }
-        str = arr.join('');
-
-        return str;
-    }
-
-    function getCounter(valueCounter: string, multiplier: string): number
+    function saveCounter(): void
     {
-        let value = valueCounter.replace(".", "");
-        let counter: number;
-
-        if (multiplier === '10') {
-            counter = Math.round(Number(value) / 10);
-        } else {
-            counter = Number(value);
-        }
-
-        return counter;
-    } 
-
-    function saveCounter2(): void // TODO
-    {
-        let counter = getCounter(valueCounter2, multiplier2);
+        let value = counter.getCounter(valueCounter2, multiplier2);
 
         setCounter2('');
-        ACTIONS.minihum_set.set(counter);
+        ACTIONS.minihum_set.set(value);
     }
 
-    const changeRadio2 = (e) => {
+    const changeRadio2 = (e: any) => {
         if (typeof e.detail.value === "string") {
             ACTIONS.smart_weather.set(e.detail.value);
         }
 	};
-
-    /**
-     * Показать счетчик
-     * @param counter - показатель счетчика
-     * @param multiplier - импульс счетчика
-     * @returns object
-     */
-    function viewCounter(counter: number, multiplier: string): object
-    {
-        let counterMultiplier = counter * Number(multiplier);
-        let arr: string[] = String(counterMultiplier).split('');
-        let str1: string, str2: string;
-
-        if (arr.length < 8) {
-            for (let i = arr.length; i < 8; i++) {
-                arr.unshift('0');
-            }
-        }
-        str1 = arr.slice(0, 5).join('');
-        str2 = arr.slice(5, 8).join('');
-        
-        return (
-            <View className={styles.indicatorCounter}>
-                <View className={styles.indicatorCounterBlock}>
-                    <Text>{str1}</Text>
-                    <Text>.</Text>
-                    <Text className={styles.threeNumber}>{str2}</Text>
-                </View>
-                <View className={styles.meterCube}>
-                    <Text>м</Text>
-                    <Text className={styles.cube}>3</Text>
-                </View>
-            </View>
-        );
-    }
 
     function viewImpuls(multiplier: string): object
     {
@@ -101,10 +36,10 @@ export default () => {
             return (
                 <React.Fragment>
                     <Label>
-                        <Radio value="1" color="#00BFFF" checked>{textImpulsLiter}</Radio>
+                        <Radio value="1" color="#00BFFF" checked>{counter.textImpulsLiter}</Radio>
                     </Label>
                     <Label>
-                        <Radio value="10" color="#00BFFF">{textImpuls10Liter}</Radio>
+                        <Radio value="10" color="#00BFFF">{counter.textImpuls10Liter}</Radio>
                     </Label>
                 </React.Fragment>
             )
@@ -112,10 +47,10 @@ export default () => {
             return (
                 <React.Fragment>
                     <Label>
-                        <Radio value="1" color="#00BFFF">{textImpulsLiter}</Radio>
+                        <Radio value="1" color="#00BFFF">{counter.textImpulsLiter}</Radio>
                     </Label>
                     <Label>
-                        <Radio value="10" color="#00BFFF" checked>{textImpuls10Liter}</Radio>
+                        <Radio value="10" color="#00BFFF" checked>{counter.textImpuls10Liter}</Radio>
                     </Label>
                 </React.Fragment>
             )
@@ -124,27 +59,35 @@ export default () => {
 
     return (
         <View>
-            <View className={styles.counter2}>
-                <Text className={styles.title}>{textIndicatorCounter}</Text>
-                <Text>2:</Text>
-                { viewCounter(counter2, multiplier2) }
-                <Text className={styles.title}>{textSettingCounter}</Text>
-                <Text>2:</Text>
-                <RadioGroup onChange={changeRadio2} options={[]} className={styles.radioGroup}>
-                    {viewImpuls(multiplier2)}
-                </RadioGroup>
-                <View className={styles.editCounter}>
-                    <Input type="digit"
-                        value={valueCounter2}
-                        maxLength={9}
-                        placeholder="00000.000"
-                        className={styles.inputNumber}
-                        onInput={handleInput2}
-                    ></Input>
-                    <Button className={styles.buttonSave} onClick={ () => {
-                        saveCounter2();
-                    }}>{textSave}</Button>
+            <Text className={styles.title}>{counter.textIndicatorCounter}</Text>
+            <Text>2:</Text>
+            <View className={styles.indicatorCounter}>
+                <View className={styles.indicatorCounterBlock}>
+                    <Text>{viewCounter[0]}</Text>
+                    <Text>.</Text>
+                    <Text className={styles.threeNumber}>{viewCounter[1]}</Text>
                 </View>
+                <View className={styles.meterCube}>
+                    <Text>м</Text>
+                    <Text className={styles.cube}>3</Text>
+                </View>
+            </View>
+            <Text className={styles.title}>{counter.textSettingCounter}</Text>
+            <Text>2:</Text>
+            <RadioGroup onChange={changeRadio2} options={[]} className={styles.radioGroup}>
+                {viewImpuls(multiplier2)}
+            </RadioGroup>
+            <View className={styles.editCounter}>
+                <Input type="digit"
+                    value={valueCounter2}
+                    maxLength={9}
+                    placeholder="00000.000"
+                    className={styles.inputNumber}
+                    onInput={handleInput2}
+                ></Input>
+                <Button className={styles.buttonSave} onClick={ () => {
+                    saveCounter();
+                }}>{counter.textSave}</Button>
             </View>
         </View>
     );
